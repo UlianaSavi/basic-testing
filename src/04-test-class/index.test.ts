e034545899b9fc356c0c8d964d121a579efc426d
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-06-28 17:18:43 +0600

    feat: finish with class tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -10,9 +10,8 @@ describe('BankAccount', () => {
   const balanse = 1000;
   const money = 50;
   const moreThanBalance = balanse + balanse;
-  // const newBalanse = balanse + money;
   const bankAccount = getBankAccount(balanse);
-  // const testAccount = getBankAccount(money);
+  const testAccount = getBankAccount(money);
 
   test('should create account with initial balance', () => {
     expect(getBankAccount(balanse)).toBeInstanceOf(BankAccount);
@@ -37,34 +36,43 @@ describe('BankAccount', () => {
   });
 
   test('should deposit money', () => {
-    // expect(bankAccount.deposit(money)).toBe(Acc with new balance);
+    bankAccount.deposit(money);
+    expect(bankAccount.getBalance()).toBe(balanse + money);
   });
 
   test('should withdraw money', () => {
-    // expect(bankAccount.withdraw(money)).toBe(Acc with new balance);
+    testAccount.withdraw(money);
+    expect(testAccount.getBalance()).toBe(money - money);
   });
 
   test('should transfer money', () => {
-    // expect(bankAccount.transfer(money, testAccount)).toBe(testAccount with new balance);
+    const accToTrans = getBankAccount(money);
+    bankAccount.transfer(money, accToTrans);
+    expect(accToTrans.getBalance()).toBe(money + money);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // await expect(bankAccount.fetchBalance()).resolves.toBe(balanse);
+    const moneyForDepos = await bankAccount.fetchBalance();
+    if (moneyForDepos) {
+      expect(moneyForDepos).toBe(moneyForDepos);
+    }
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // const testCase = await bankAccount.fetchBalance();
-    // if (testCase) {
-    //   expect(bankAccount.deposit(money)).toBe('Acc with new balance');
-    // }
+    const accToDepos = getBankAccount(money);
+    const moneyForDepos = await accToDepos.fetchBalance();
+    if (moneyForDepos) {
+      expect(accToDepos.deposit(moneyForDepos)).toBe({
+        _balance: money + moneyForDepos,
+      });
+    }
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    const testCase = await bankAccount.fetchBalance();
-    if (!testCase) {
-      const synchronizeBalance = await bankAccount.synchronizeBalance();
-      expect(() => {
-        synchronizeBalance;
+    const moneyForDepos = await bankAccount.fetchBalance();
+    if (!moneyForDepos) {
+      expect(async () => {
+        await bankAccount.synchronizeBalance();
       }).rejects.toThrow(new SynchronizationFailedError());
     }
   });
